18e9cd2e38f18d7031109a07f56e3b49b3db7a80
angel-dominguez-portillo <L24550319@chihuahua2.tecnm.mx>
2026-02-09 17:17:35 -0600

    Update EVA1_8_ARREGLOS.java

--- EVA1_8_ARREGLOS/src/EVA1_8_ARREGLOS/EVA1_8_ARREGLOS.java
+++ EVA1_8_ARREGLOS/src/EVA1_8_ARREGLOS/EVA1_8_ARREGLOS.java
@@ -2,17 +2,18 @@ package EVA1_8_ARREGLOS;
 
 public class EVA1_8_ARREGLOS {
 	public static void main(String args[]){
-		int[] datos = new int[1000000000];//-----> 4 gigas
+		int[] datos = new int[10];//-----> 4 gigas
 		
 		
 		for(int i = 0; i < datos.length; i++) {
 			datos[i] = (int)(Math.random() * 100);
 		}
 		
-		/*for(int i = 0; i < datos.length; i++) {
+		for(int i = 0; i < datos.length; i++) {
 			System.out.print("[" + datos[i] + "]");
 		}
-		System.out.println("");*/
+		System.out.println("");
 	}
 
 }
+
